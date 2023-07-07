923d036f644fbf6e4f71ac53f9a4edc5487a423c
Anyikabs <114746509+Anyikabs@users.noreply.github.com>
2023-07-07 10:37:13 +0100

    Fixed

--- console.py
+++ console.py
@@ -2,8 +2,12 @@
 """ Console Module """
 import cmd
 import sys
+import re
+import os
+from datetime import datetime
+import uuid
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
 from models.user import User
 from models.place import Place
 from models.state import State
@@ -65,7 +69,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline.partition(', ')  # pline convert to tuple
 
                 # isolate _id, stripping quotes
-                _id = pline[0].replace('"', '')
+                _id = pline[0].replace('\"', '')
                 # possible bug here:
                 # empty quotes register as empty _id when replaced
 
@@ -94,7 +98,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, command):
         """ Method to exit the HBNB console"""
-        exit()
+        exit(0)
 
     def help_quit(self):
         """ Prints the help documentation for quit  """
@@ -103,7 +107,7 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, arg):
         """ Handles EOF to exit program """
         print()
-        exit()
+        exit(0)
 
     def help_EOF(self):
         """ Prints the help documentation for EOF """
@@ -115,56 +119,62 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        ignored_attrs = ('id', 'created_at', 'updated_at', '__class__')
+        class_name = ''
+        name_pattern = r'(?P<name>(?:[a-zA-Z]|_)(?:[a-zA-Z]|\d|_)*)'
+        class_match = re.match(name_pattern, args)
+        obj_kwargs = {}
+        if class_match is not None:
+            class_name = class_match.group('name')
+            params_str = args[len(class_name):].strip()
+            params = params_str.split(' ')
+            str_pattern = r'(?P<t_str>"([^"]|\")*")'
+            float_pattern = r'(?P<t_float>[-+]?\d+\.\d+)'
+            int_pattern = r'(?P<t_int>[-+]?\d+)'
+            param_pattern = '{}=({}|{}|{})'.format(
+                name_pattern,
+                str_pattern,
+                float_pattern,
+                int_pattern
+            )
+            for param in params:
+                param_match = re.fullmatch(param_pattern, param)
+                if param_match is not None:
+                    key_name = param_match.group('name')
+                    str_v = param_match.group('t_str')
+                    float_v = param_match.group('t_float')
+                    int_v = param_match.group('t_int')
+                    if float_v is not None:
+                        obj_kwargs[key_name] = float(float_v)
+                    if int_v is not None:
+                        obj_kwargs[key_name] = int(int_v)
+                    if str_v is not None:
+                        obj_kwargs[key_name] = str_v[1:-1].replace('_', ' ')
+        else:
+            class_name = args
+        if not class_name:
             print("** class name missing **")
             return
-        # split arguments into an array ie
-        # ['class', 'key=value', 'key=value' ...]
-        split_args = args.split()
-        if split_args[0] not in HBNBCommand.classes:
+        elif class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-
-        # separate class name from parameters
-        class_name = split_args[0]
-        # remove class name from parameters
-        split_args.pop(0)
-
-        dic = {}
-        # check if the list of parameters is not empty
-        if len(split_args) != 0:
-            for i in range(len(split_args)):
-                key_value = split_args[i].split('=')
-                key = key_value[0]
-                value = key_value[1]
-
-                # if value is a string
-                if value.startswith('"') and value.endswith('"'):
-                    value = value.replace('_', ' ')
-                    value = value.strip('"')
-                    dic[key] = str(value)
-                # if value is a float
-                elif '.' in value:
-                    try:
-                        dic[key] = float(value)
-                    except ValueError:
-                        continue
-                # if value is an integer
-                else:
-                    try:
-                        dic[key] = int(value)
-                    except ValueError:
-                        continue
-        # check if state has any parameters
-        if len(dic) == 0:
-            new_instance = HBNBCommand.classes[class_name]()
+        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            if not hasattr(obj_kwargs, 'id'):
+                obj_kwargs['id'] = str(uuid.uuid4())
+            if not hasattr(obj_kwargs, 'created_at'):
+                obj_kwargs['created_at'] = str(datetime.now())
+            if not hasattr(obj_kwargs, 'updated_at'):
+                obj_kwargs['updated_at'] = str(datetime.now())
+            new_instance = HBNBCommand.classes[class_name](**obj_kwargs)
+            new_instance.save()
+            print(new_instance.id)
         else:
             new_instance = HBNBCommand.classes[class_name]()
-            for key, value in dic.items():
-                setattr(new_instance, key, value)
-        storage.new(new_instance)
-        storage.save()
-        print(new_instance.id)
+            for key, value in obj_kwargs.items():
+                if key not in ignored_attrs:
+                    setattr(new_instance, key, value)
+            new_instance.save()
+            print(new_instance.id)
 
     def help_create(self):
         """ Help information for the create method """
@@ -195,7 +205,7 @@ class HBNBCommand(cmd.Cmd):
 
         key = c_name + "." + c_id
         try:
-            print(storage._FileStorage__objects[key])
+            print(storage.all()[key])
         except KeyError:
             print("** no instance found **")
 
@@ -227,7 +237,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del (storage.all()[key])
+            storage.delete(storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -237,27 +247,9 @@ class HBNBCommand(cmd.Cmd):
         print("Destroys an individual instance of a class")
         print("[Usage]: destroy <className> <objectId>\n")
 
-    # def do_all(self, args):
-    #     """ Shows all objects, or all objects of a class"""
-    #     print_list = []
-
-    #     if args:
-    #         args = args.split(' ')[0]  # remove possible trailing args
-    #         if args not in HBNBCommand.classes:
-    #             print("** class doesn't exist **")
-    #             return
-    #         for k, v in storage.all().items():
-    #             if k.split('.')[0] == args:
-    #                 print_list.append(str(v))
-    #     else:
-    #         for k, v in storage.all().items():
-    #             print_list.append(str(v))
-
-    #     print(print_list)
-
     def do_all(self, args):
-        """Shows all objects, or all objects of a class"""
-        obj_list = []
+        """ Shows all objects, or all objects of a class"""
+        print_list = []
 
         if args:
             args = args.split(' ')[0]  # remove possible trailing args
@@ -266,12 +258,12 @@ class HBNBCommand(cmd.Cmd):
                 return
             for k, v in storage.all().items():
                 if k.split('.')[0] == args:
-                    obj_list.append(v.to_dict())
+                    print_list.append(str(v))
         else:
             for k, v in storage.all().items():
-                obj_list.append(v.to_dict())
+                print_list.append(str(v))
 
-        print(obj_list)
+        print(print_list)
 
     def help_all(self):
         """ Help information for the all command """
@@ -281,7 +273,7 @@ class HBNBCommand(cmd.Cmd):
     def do_count(self, args):
         """Count current number of class instances"""
         count = 0
-        for k, v in storage._FileStorage__objects.items():
+        for k, v in storage.all().items():
             if args == k.split('.')[0]:
                 count += 1
         print(count)
