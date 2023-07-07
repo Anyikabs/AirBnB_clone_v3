2f644486823624d0de8f974c83ea48e5467283da
Anyikabs <114746509+Anyikabs@users.noreply.github.com>
2023-07-07 12:15:38 +0100

    Updated

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,49 @@
-#!/usr/bin/env python3
-"""
-This script contains the function do_deploy
-that distributes an archive to our web servers
-"""
+#!/usr/bin/python3
+# Fabfile to distribute an archive to a web server.
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-from fabric.api import put, run, env
-import os
+env.hosts = ["104.196.168.90", "35.196.46.172"]
 
-env.user = "ubuntu"
-env.hosts = ["54.87.210.50", "54.82.137.30"]
 
 def do_deploy(archive_path):
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-    This function distributes an archive to my webservers
-    """
-
-    if not os.path.exists(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    archive = os.path.basename(archive_path)
-    archive_wout_exten = os.path.splitext(archive)[0]
-
-    if put(archive_path, "/tmp/{}".format(archive)).failed:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-
-    if run("mkdir -p /data/web_static/releases/{}/".format(archive_wout_exten)).failed:
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive, archive_wout_exten)).failed:
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-
-    if run("rm /tmp/{}".format(archive)).failed:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
         return False
-
-    if run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(archive_wout_exten, archive_wout_exten)).failed:
+    if run("rm /tmp/{}".format(file)).failed is True:
         return False
-
-    if run("rm -rf /data/web_static/releases/{}/web_static/".format(archive_wout_exten)).failed:
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-
-    if run("rm -rf /data/web_static/current").failed:
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
         return False
-
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(archive_wout_exten)).failed:
+    if run("rm -rf /data/web_static/current").failed is True:
         return False
-
-    print("New version deployed!")
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
     return True
-
