31dcf70fc74e65b1978d0b752283237b820fd5a8
Anyikabs <anyikabsdev@gmail.com>
2023-07-07 13:42:01 -0700

    updates

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,14 +1,26 @@
-# Configures the webserver for deployment of web_static
+# Configures a web server for deployment of web_static.
 
+# Nginx configuration file
 $nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By ${hostname};
     root   /var/www/html;
     index  index.html index.htm;
+
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
     }
 }"
 
@@ -39,7 +51,7 @@ file { '/data/web_static/shared':
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "<html><head><Sample></head><body><h1>Sample Page</h1></body></html>"
+  content => "Holberton School Puppet\n"
 } ->
 
 file { '/data/web_static/current':
@@ -50,6 +62,29 @@ file { '/data/web_static/current':
 exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
+
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
 
 exec { 'nginx restart':
   path => '/etc/init.d/'
