0cb4ffb01ad88b5c7d1d10078dd57715e0c14f8b
Anyikabs <114746509+Anyikabs@users.noreply.github.com>
2023-07-07 11:18:19 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,27 +4,32 @@ Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
-from fabric.api import put, run, env
+from fabric.api import env, put, run
 from os.path import exists
+
 env.hosts = ['54.87.210.50', '54.82.137.30']
 
-
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    if not exists(archive_path):
         return False
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+        # Get the file name without extension
+        file_name = archive_path.split("/")[-1]
+        name = file_name.split(".")[0]
+        # Uncompress the archive to the folder on the web server
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file_name, name))
+        # Delete the archive from the web server
+        run("rm /tmp/{}".format(file_name))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        # Delete the symbolic link from the web server
+        run("rm -rf /data/web_static/current")
+        # Create a new the symbolic link on the web server
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
         return True
     except:
         return False
