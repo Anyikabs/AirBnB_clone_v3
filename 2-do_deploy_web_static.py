84997cb6b2703b597f86d893bdec52fca3564617
Anyikabs <anyikabsdev@gmail.com>
2023-07-06 20:18:19 -0700

    ip fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["54.87.210.50", "54.82.137.30"]
 env.user = "ubuntu"
 
 
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["54.87.210.50", "54.82.137.30"]
 
 
 def do_pack():
